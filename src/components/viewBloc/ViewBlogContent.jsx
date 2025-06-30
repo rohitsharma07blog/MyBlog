948f6290a3c8dd88b657cbc4faf70ca1c872e9be
Raj Sharma <rajrsharma2004@gmail.com>
2025-07-01 00:45:00 +0530

    markdown error

--- src/components/viewBloc/ViewBlogContent.jsx
+++ src/components/viewBloc/ViewBlogContent.jsx
@@ -8,7 +8,7 @@ export default function ViewBlogContent() {
         <div id="progress" className="h-1 bg-white shadow" style={{background: "linear-gradient(to right, #4dc0b5 var(--scroll), transparent 0)"}}></div>
         
         <div className="prose max-w-3xl mx-auto p-4">
-            <ReactMarkdown>{blog.content}</ReactMarkdown>
+            <ReactMarkdown children={blog.content}></ReactMarkdown>
         </div>
         </>
     );
