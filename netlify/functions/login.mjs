bfb96f4c439dab5935fe6ae3b301a10b86dac4d9
Raj Sharma <rajrsharma2004@gmail.com>
2025-07-01 16:17:02 +0530

    Login Function

---
+++ netlify/functions/login.mjs
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const SECRET = process.env.JWT_SECRET;
+const userName = process.env.USER_NAME;
+const password = process.env.PASSWORD;
+
+export default async (req, context) =>{
+    if(req.method !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ error: 'Method Not Allowed' })
+        };
+    }
+    const { username, password: userPassword } = JSON.parse(req.body);
+    if (username === userName && userPassword === password) {
+        const token = jwt.sign({ username }, SECRET, { expiresIn: '5h' });
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ token })
+        };
+    }
+    return {
+        statusCode: 401,
+        body: JSON.stringify({ error: 'Unauthorized' })
+    };
+}
