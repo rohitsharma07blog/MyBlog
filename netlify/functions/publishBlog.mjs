4dac42ed3a450fbec1b1351c8cb63de1e9142530
Raj Sharma <rajrsharma2004@gmail.com>
2025-07-08 23:17:21 +0530

    Logs

--- netlify/functions/publishBlog.mjs
+++ netlify/functions/publishBlog.mjs
@@ -85,7 +85,7 @@ export default async (req, context) => {
 
     // Verify JWT
     const token = req.headers.authorization?.split(' ')[1];
-    console.log('Received token:', token);
+    console.log('Received token:', req.headers);
     if (!token) {
         return new Response(
             JSON.stringify({ error: 'Unauthorized' }),
