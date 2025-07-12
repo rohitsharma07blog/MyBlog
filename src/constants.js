c75f0c7ca78b9a26ecce46b2ecb216aca10e5a55
rohitsharma07blog <rohitsharma07.blog@gmail.com>
2025-07-12 17:50:44 +0000

    profile update

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,11 @@
 export const NAME = "Rohit Sharma";
-export const EMAIL = "rohit.sharma@gmail.com";
-export const PROFILE_IMAGE = "https://res.cloudinary.com/dgeav4iqp/image/upload/v1751712877/Screenshot_2025-06-19_235827_qdxef7.png";
-export const ABOUT = "Passionate computer engineering student with a keen interest in web development and software engineering. I love creating user-friendly applications and solving complex problems. Always eager to learn new technologies and improve my skills.";
-export const PHONE = "+91 12345 67890";
-export const LOCATION = "PUNE, INDIA";
-export const LINKEDIN = "https://www.linkedin.com/in/rohit-sharma";
+export const EMAIL = "rohit21053@gmail.com";
+export const PROFILE_IMAGE = "https://res.cloudinary.com/do1iivnq7/image/upload/v1752342272/WhatsApp_Image_2025-07-12_at_11.03.34_PM_lk3dou.jpg";
+export const ABOUT = ``` Welcome to my history website, where I personally explore and share fascinating stories from the ancient world to modern times. As the sole writer and researcher behind every article, I am passionate about bringing history to life through well-researched, engaging, and accessible content.
+
+My goal is to provide readers with a comprehensive journey through humanity’s past—covering key events, influential civilizations, and remarkable individuals who have shaped the course of history. Each article reflects my dedication to accuracy, depth, and storytelling that connects the past with today’s world.
+
+Thank you for visiting and joining me on this adventure through time. I hope my work inspires your curiosity and deepens your appreciation for the rich and diverse history that surrounds us all.```;
+export const PHONE = "7030659297";
+export const LOCATION = "Mumbai, India";
+export const LINKEDIN = "https://www.linkedin.com/in/rohit-sharma-45bb0633b";
