d98a36bb08513edcd7b14b5c73be05f52ea65120
rohitsharma07blog <rohitsharma07.blog@gmail.com>
2025-07-12 16:07:12 +0000

    Logo Updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header(){
        <header className="w-full">
   <nav className="border-gray-200 bg-gray-900 py-2.5">
     <div className="mx-auto flex max-w-screen-xl flex-wrap items-center px-4">
-      <img className="w-10 h-10 rounded-md mr-5" src="/public/blog_logo.svg" alt="Rounded avatar"></img>
+      <img className="w-10 h-10 rounded-md mr-5" src="/blog_logo.svg" alt="Rounded avatar"></img>
       <a href="#" className="flex items-center"><span className="self-center text-xl font-semibold whitespace-nowrap text-white">{NAME}</span></a>
       
     </div>
